008242c0b6c7b2fc7219ccf44ba4f456896715eb
amoufaq5 <137143672+amoufaq5@users.noreply.github.com>
2025-03-21 19:06:16 +0200

    Create db_config.py

---
+++ data_pipeline/db_config.py
@@ -0,0 +1,14 @@
+# data_pipeline/db_config.py
+
+import os
+from sqlalchemy import create_engine
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "password")
+DB_NAME = os.getenv("DB_NAME", "medical_db")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = os.getenv("DB_PORT", "5432")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=False)
